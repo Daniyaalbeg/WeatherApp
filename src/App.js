934f2131e6458618d0d1073dba6df5e7764d350c
Thomas Bunyan <t.bunyan@se16.qmul.ac.uk>
2018-02-23 18:05:31 +0000

    Added component template

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import DogInterface from './DogInterface.js'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <h1 className="App-title">Welcome</h1>
-                </header>
+                <div className="doginterface">
+                    <DogInterface/>
+                </div>
+                <div className="weatherbar">
+                    <DogInterface/>
+                </div>
+                <div className="settings">
+                    <DogInterface/>
+                </div>
             </div>
             );
         }
---
+++ src/DogInterface.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DogInterface.css';
+
+class DogInterface extends Component {
+    render() {
+        return (
+            <div className="DI">
+                <p>Hello World</p>
+            </div>
+            );
+        }
+    }
+
+    export default DogInterface;
