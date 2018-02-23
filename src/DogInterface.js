6cc65f2d4e3de45ea1758d7d9cc3aecb02636f6c
Thomas Bunyan <t.bunyan@se16.qmul.ac.uk>
2018-02-23 19:05:07 +0000

    Changes to dog interface component

--- src/DogInterface.js
+++ src/DogInterface.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
 import './DogInterface.css';
 
-class DogInterface extends Component {
-    render() {
-        return (
-            <div className="DI">
-                <p>Hello World</p>
-            </div>
+    class DogInterface extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                date: new Date(),
+                color_black: true
+            };
+
+        }
+        componentDidMount() {
+            this.timerID = setInterval(
+              () => this.tick(),
+              1000
+            );
+        }
+        componentWillUnmount() {
+            clearInterval(this.timerID);
+        }
+        tick() {
+            this.setState({
+              date: new Date()
+            });
+        }
+
+        changeColor(){
+            this.setState({color_black: !this.state.color_black})
+        }
+
+        render() {
+            let bgColor = this.state.color_black ? "white" : "black";
+
+            return (
+              <div style={{color: bgColor}} onClick={this.changeColor.bind(this)}>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+              </div>
             );
         }
     }
