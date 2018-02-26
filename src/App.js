a2b4e34729dbd713f63a74f72d5ad4decab5248e
Tom <t.bunyan@se16.qmul.ac.uk>
2018-02-26 18:41:21 +0000

    A lot of UI changes and file organisation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import DogInterface from './DogInterface.js'
-import WeatherBar from './WeatherBar.js'
-import Settings from './Settings.js'
+import DogInterface from './DogInterface/DogInterface.js'
+import WeatherBar from './WeatherBar/WeatherBar.js'
+import Settings from './Settings/Settings.js'
 import './App.css';
 
 class App extends Component {
---
+++ src/WeatherBar/WeatherBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './WeatherBar.css';
+
+class WeatherBar extends Component {
+    render() {
+        return (
+            <div className="wb">
+                <p>WeatherBar!</p>
+            </div>
+            );
+        }
+    }
+
+    export default WeatherBar;
