2aa0bcba94dec6c45d8b7f146e5576f95af6f15b
Kaamil Hussain Jasani <k.jasani@se16.qmul.ac.uk>
2018-03-02 18:37:48 +0000

    Added loader (not integrated yet) (actually this time)

---
+++ src/Loader/Loader.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './Loader.css';
+
+class Loader extends Component{
+    render(){
+        return(
+            <div id="loader"></div>
+        );
+    }
+}
+export default Loader;
