7168cfa6025b1aa944f2f3959b355fe11e1a1472
Kaamil Hussain Jasani <k.jasani@se16.qmul.ac.uk>
2018-03-02 18:33:12 +0000

    Added loader, not integrated yet.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import WeatherData from './WeatherData.js'
-import Settings from './Settings/Settings.js'
+import WeatherData from './WeatherData.js';
+import Settings from './Settings/Settings.js';
 import './App.css';
 
 class App extends Component {
